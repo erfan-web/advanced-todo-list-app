eb7aa35a768f3aaa3246073591baf99a6d84dd13
erfan-web <erfan.dev85@gmail.com>
2025-08-28 23:57:03 +0330

    fix(theme): persist selected theme in localStorage

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     const newTheme = currentTheme == "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme" , newTheme)
     updateThemeIcon(newTheme);
   }
   function updateThemeIcon(theme) {
